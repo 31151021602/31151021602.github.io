19bb1393ac1dbfa2436593c8847121c76b460cbc
31151021602 <30387688+31151021602@users.noreply.github.com>
2018-05-20 02:17:15 +0700

    31151021602

---
+++ JS/BT3.js
@@ -0,0 +1,97 @@
+﻿
+function kiemtra() {
+    var loi = true;
+    hoten = document.getElementById("name");
+    ht = hoten.value;
+    if (ht == "") {
+        document.getElementById("errname").innerHTML = "Họ tên không được để trống ";
+        hoten.style.backgroundColor = "yellow";
+        loi = (loi && false);
+    }
+    else {
+        document.getElementById("errname").innerHTML = "";
+        hoten.style.backgroundColor = "Green"
+    }
+    ////email
+    //email = document.getElementById("gmail");
+    //g = email.value;
+    //var gt = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    //if (!gt.test(g)) {
+    //    document.getElementById("errmail").innerHTML = "Email không được để trống hoặc bạn đã nhập sai email";
+    //    email.style.backgroundColor = "yellow";
+    //    loi = (loi && false);
+    //}
+    //else {
+    //    document.getElementById("errmail").innerHTML = "";
+    //    email.style.backgroundColor = "Green"
+    //}
+
+    //sdt
+    sodt = document.getElementById("sdt");
+    dt = sodt.value;
+    var khonghople = /^[0-9-+]+$/;
+    if (!khonghople.test(dt)) {
+        document.getElementById("errsdt").innerHTML = "Số điện thoại không được để trống hoặc số điện này có chứa một số ký tự không phù hợp";
+        sodt.style.backgroundColor = "yellow";
+        loi = (loi && false);
+    }
+
+    else {
+        document.getElementById("errsdt").innerHTML = "";
+        sodt.style.backgroundColor = "Green"
+    }
+    //tinh
+    tinhtp = document.getElementById("tinh");
+    tt = tinhtp.value;
+    if (tt == "") {
+        document.getElementById("errtinh").innerHTML = "Vui lòng chọn tỉnh thành phố để xác định vị trí của bạn";
+        tinhtp.style.backgroundColor = "yellow";
+        loi = (loi && false);
+
+    }
+    else {
+        document.getElementById("errtinh").innerHTML = "";
+        tinhtp.style.backgroundColor = "Green"
+    }
+    //duong
+    duongdc = document.getElementById("duong");
+    dg = duongdc.value;
+    if (dg == "") {
+        document.getElementById("errduong").innerHTML = "Đường không được để trống không được để trống ";
+        duongdc.style.backgroundColor = "yellow";
+        loi = (loi && false);
+    }
+    else {
+        document.getElementById("errduong").innerHTML = "";
+        duongdc.style.backgroundColor = "Green"
+    }
+    //diachi
+    d_chi = document.getElementById("diachi");
+    dc = d_chi.value;
+    if (dc == "") {
+        document.getElementById("errdiachi").innerHTML = "Đại chỉ không được để trống không được để trống ";
+        d_chi.style.backgroundColor = "yellow";
+        loi = (loi && false);
+    }
+    else {
+        document.getElementById("errdiachi").innerHTML = "";
+        d_chi.style.backgroundColor = "Green"
+    }
+    //kiểm tra magiamgia
+    gg = document.getElementById("magg");
+    ma = gg.value
+    var khonghp = /^[0-9-+]+$ /;
+    if (!khonghople.test(ma)) {
+        document.getElementById("errgg").innerHTML = "Mã giảm giá này không hợp lệ";
+        gg.style.backgroundColor = "yellow";
+        loi = (loi && false);
+    } else {
+        document.getElementById("errgg").innerHTML = "";
+        gg.style.backgroundColor = "Green"
+    }
+    if (loi == true) {
+        document.getElementById("myform").submit();
+        alert("Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi! ^^");
+    }
+
+}
---
+++ JS/Cau1.js
@@ -0,0 +1,48 @@
+﻿function taosongaunhien() {
+    value1 = parseInt(document.getElementsByName("giatri")[0].value);
+    a = parseInt(Math.random() * (value1) + 1);
+    document.getElementsByName("kq")[0].value = a;
+
+}
+function taodayngaunhien() {
+    document.getElementsByName("kq")[0].value = '';
+    a = parseInt(document.getElementsByName("giatri")[0].value);
+
+    var b = new Array(a);
+    var c = '';
+    var m = '';
+    var n = '';
+    var tam = '';
+    for (var i = 0; i < b.length; i++) {
+        b[i] = i + 1;
+    }
+    for (var j = 0; j < b.length; j++) {
+        m = parseInt(Math.random() * (a));
+        n = parseInt(Math.random() * (a));
+        tam = b[m];
+        b[m] = b[n];
+        b[n] = tam;
+    }
+    for (var k = 0; k < b.length; k++) {
+        if (k == b.length - 1) {
+            c += b[k] ;
+
+        }
+        else {
+            c += b[k] + ',';
+        }
+        
+    }
+    
+    document.getElementsByName("kq")[0].value += c;
+} function xoso() {
+    for (var i = 1; i <= 300; i++) {
+        setTimeout(taosongaunhien, 50 * i);
+    }
+}
+
+function xoso1() {
+    for (var i = 1; i <= 300; i++) {
+        setTimeout(taodayngaunhien, 50 * i);
+    }
+}
